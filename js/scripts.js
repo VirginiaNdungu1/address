3ddb52c627f6fdbb6635651f8599e4f9869c9a31
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-08-24 10:43:51 +0300

    Update business logic Create addresses constructor Create Contact object Create addresses object

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 //business logic
+//create Contact constructor
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
 }
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
+}
+//create Addresses constructor
+function Addresses(street, city, county) {
+  this.street = street;
+  this.city = city;
+  this.county = county;
 }
 // user interface logic
 
@@ -14,6 +22,13 @@ $(document).ready(function() {
     var userFirstName = $("input#new-firstName").val();
     var userLastName = $("input#new-lastName").val();
     var newContact = new Contact(userFirstName, userLastName);
+    var jane = new Contact("Jane", "Njeri");
+    console.log(jane);
+    var home = new Addresses("671 Ngong Road", "Nairobi", "Nairobi");
+    console.log(home);
+    jane.addresses.push(home);
+    console.log(jane);
+    console.log(jane.addresses);
 
     $("ul#contacts").append("<li><span class='contact'> " + newContact.fullName() + "</span></li>");
     //shows contact when it is clicked.
